ac709bb3f6476f71fc0fd2dd4ca4f198e8ee187a
Max-le <max.09@outlook.com>
2018-10-03 11:55:22 +0200

    find value

--- Week7/MinMax.java
+++ Week7/MinMax.java
@@ -1,16 +1,55 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 
 public class MinMax{
 
 
-public static int[] find(int[] list, int goal){
+	public static int[] find(int[] list, int goal){
 	//Base step 
-	
+		if (list.length == 1 ){
+
+			if (list[0] == goal) {
+				System.out.println("Found it ! ");
+				return list;
+			}
+			else {
+				System.out.println(goal +" was not found in the list");
+				return list;
+			}
+
+		}
+
+		//Recursive call
+		if(list[0]!= goal){
+			System.out.println(list[0] + " != from goal...");
+
+			//Remove item from list
+			System.out.println("list :"+Arrays.toString(list));
+
+			int[] newlist = new int[list.length-1];
+			System.arraycopy(list,1,newlist,0,list.length-1);
+			System.out.println("newlist :"+Arrays.toString(newlist));
+
+
+			return find(newlist, goal);
+
+
+		}
+		else {
+			//Item found
+			//returns list with only solution in it.
+			int[] newlist = new int[1];
+			newlist[0]=list[0]; // list[0]==goal 
+
+
+			return find(newlist, goal) ;
+		}
+
+	}//End find()
 
 
 
-}
 	public static int Min(int[] list){
 
 		//Base step
@@ -18,17 +57,17 @@ public static int[] find(int[] list, int goal){
 			return list[0];
 		}
 
-			if (list[0] < list[1] ){
+		if (list[0] < list[1] ){
 				//
-				list[1]= list[0];
+			list[1]= list[0];
 
 
-			}
+		}
 
-			int[] newlist = new int[list.length-1];
-			System.arraycopy(list,1,newlist,0,list.length-1);
+		int[] newlist = new int[list.length-1];
+		System.arraycopy(list,1,newlist,0,list.length-1);
 
-			return Min(newlist);
+		return Min(newlist);
 
 
 		
@@ -48,18 +87,25 @@ public static int[] find(int[] list, int goal){
 
 
 		}
-			int[] newlist = new int[list.length-1];
-			System.arraycopy(list,1,newlist,0,list.length-1);
-			return Max(newlist);
+		int[] newlist = new int[list.length-1];
+		System.arraycopy(list,1,newlist,0,list.length-1);
+		return Max(newlist);
 
 	}
 
 	public static void main(String[] args) {
 
-		int[] list = {54,34,6,35};
+		int[] list = {54,-35,6,394,0};
 
-		System.out.println("Minimum = "+Min(list));
-		System.out.println("Max = "+Max(list));
+		// System.out.println("Minimum = "+Min(list));
+		// System.out.println("Max = "+Max(list));
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the value you're looking for.");
+		int goal = sc.nextInt();
+		find(list, goal);
+
+
+
 
 
 		
