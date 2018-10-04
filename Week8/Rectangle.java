486d6cc3d4176aec7f17ff865362025d1b67e4a0
Max-le <max.09@outlook.com>
2018-10-04 19:12:00 +0200

    finish rectangle

--- Week8/Rectangle.java
+++ Week8/Rectangle.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Rectangle {
 
 
@@ -20,12 +22,43 @@ public class Rectangle {
 		return width * height; 
 	}
 
+	//Getters
+	public double getHeight(){
+		return height; 
+	}
+	public double getWidth(){
+		return width;
+	}
+
+	//Setters 
+	public void setHeight(double height){
+		this.height = height;
+	}
+	public void setWitdh(){
+		//Another way to set the value
+		System.out.println("Enter the width : ");
+		Scanner sc = new Scanner(System.in);
+		this.width = sc.nextInt();
+	}
+	
+
+
 
 ////Test client
 public static void main(String[] args) {
-	Rectangle r1 = new Rectangle(2.3, 3.3) ;
-	System.out.println(r1.width + " "+r1.height);
+	Rectangle r1 = new Rectangle(2.3, 3.3);
+
+	System.out.println(r1.width + " "+r1.height );
 	System.out.println("Aera : "+ r1.area()); 
+	System.out.println(r1.getWidth());
+	System.out.println(r1.getHeight());
+	r1.setHeight(42);
+	r1.setWitdh(); 
+	System.out.println(r1.area()); 
+
+
+
+
 }
 
 }
